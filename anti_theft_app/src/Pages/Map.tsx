6e34a70d618f47ff913f70c1ae0329b081576bd8
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-12-30 21:16:10 +0530

    Update logo file reference in Map component

--- anti_theft_app/src/Pages/Map.tsx
+++ anti_theft_app/src/Pages/Map.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/LOGO.png";
 import MapView from "../Components/LeafletMap";
 
 export default function Map() {
