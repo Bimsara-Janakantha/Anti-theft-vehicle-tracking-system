59f15a47c370d316ee61316dbc93674e2ab23382
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-12-30 17:49:07 +0530

    Refactor LeafletMap component for improved initialization and remove unused Markers component

--- anti_theft_app/src/Components/LeafletMap.tsx
+++ anti_theft_app/src/Components/LeafletMap.tsx
@@ -1,22 +1,26 @@
 import { useEffect, useRef } from "react";
-import L from "leaflet";
+import L, { LatLngExpression } from "leaflet";
 
 export default function LeafletMap() {
   const mapRef = useRef<L.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const location: LatLngExpression = [7.255020712394069, 80.59179042409036];
+  const zoomLevel = 17;
 
   useEffect(() => {
-    if (mapRef.current) return; // prevent double init
+    if (!mapContainerRef.current || mapRef.current) return;
 
-    mapRef.current = L.map("map").setView(
-      [7.2906, 80.6337], // Kandy
-      13
+    mapRef.current = L.map(mapContainerRef.current).setView(
+      location,
+      zoomLevel
     );
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "&copy; OpenStreetMap contributors",
     }).addTo(mapRef.current);
 
-    L.marker([7.2906, 80.6337])
+    L.marker(location)
       .addTo(mapRef.current)
       .bindPopup("University of Peradeniya");
 
@@ -24,7 +28,10 @@ export default function LeafletMap() {
       mapRef.current?.remove();
       mapRef.current = null;
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div id="map" style={{ height: "100%", width: "100%" }} />;
+  return (
+    <div ref={mapContainerRef} style={{ height: "100%", width: "100%" }} />
+  );
 }
